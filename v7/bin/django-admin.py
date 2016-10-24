6d0da1dd2b395f4c262cbaef95a0783a87358f59
Alexandre Proença <alexandre.proenca@hotmail.com.br>
2016-10-24 00:15:51 -0200

    v0.4.1

--- v7/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/smart/PycharmProjects/django-auto-rest-project/v7/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- v7/morando/morando/settings/__init__.py
+++
@@ -1,3 +0,0 @@
-# coding: utf-8
-from dev import *
-#from production import *
